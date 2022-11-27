c39c98d6e471f8e98c54af45c6d12be48615fbbe
ZoltanKuli <zoltan.oa@gmail.com>
2022-11-27 12:06:20 +0100

    Add New Game Button Animation

--- app/src/main/java/com/example/minimalisthangman/game/menu/MenuView.kt
+++ app/src/main/java/com/example/minimalisthangman/game/menu/MenuView.kt
@@ -1,19 +1,25 @@
 package com.example.minimalisthangman.game.menu
 
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import com.example.minimalisthangman.R
 import com.example.minimalisthangman.game.GameActivity
 
+
 class MenuView(private val gameActivity: GameActivity) : View.OnClickListener {
 
     private val newGameButton: ImageButton = gameActivity.findViewById(R.id.newGameButton)
+    private val newGameButtonAnimation: Animation =
+        AnimationUtils.loadAnimation(gameActivity.applicationContext, R.anim.new_game_button_rotate)
 
     init {
         newGameButton.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
+        newGameButton.startAnimation(newGameButtonAnimation)
         gameActivity.startNewGame()
     }
 }
